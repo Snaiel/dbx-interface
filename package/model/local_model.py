704a85f2cf64c993ff04192d2dec3ff83d04a2d6
Snaiel <56014779+Snaiel@users.noreply.github.com>
2023-09-13 18:37:12 +0800

    actually ignore directories

--- package/model/local_model.py
+++ package/model/local_model.py
@@ -4,6 +4,7 @@ from typing import Callable
 from package.model.interface_model import InterfaceModel, ExplorerTask
 from package.model.dbx_model import DropboxModel
 from package.utils import read_config, TIMESTAMP_FORMAT
+from pprint import pprint
 
 colorama.init(autoreset=True)  # Automatically reset colors after each print
 
@@ -95,7 +96,18 @@ class LocalModel(InterfaceModel):
         gitignore_stack: list[tuple[str, pathspec.PathSpec]] = []
         applicable_gitignores: list[pathspec.PathSpec] = []
 
+        ignored_directories: list[str] = []
+        ignored_dirs_stack: list[int] = []
+
         for dirpath, dirnames, filenames in os.walk(self.local_root):
+            skip_dir = False
+            for dir in ignored_directories:
+                if dirpath.startswith(dir):
+                    skip_dir = True
+                    break
+            if skip_dir:
+                continue
+
             # consider gitignore files
             if '.gitignore' in filenames:
                 gitignore_path = os.path.join(dirpath, '.gitignore')
@@ -103,54 +115,87 @@ class LocalModel(InterfaceModel):
                     spec = pathspec.PathSpec.from_lines(pathspec.patterns.GitWildMatchPattern, file.readlines())
                     gitignore_stack.append((gitignore_path, spec))
 
-            applicable_gitignores = self._get_applicable_gitignores(dirpath, gitignore_stack)
+                new_ignored_dicts = []
 
-            for filename in filenames:
-                # construct the full local path
-                file_local_path = os.path.join(dirpath, filename)
-                file_relative_path = "/" + os.path.relpath(file_local_path, self.local_root)
+                for dirname in dirnames:
+                    if spec.match_file(dirname):
+                        new_ignored_dicts.append(os.path.join(dirpath, dirname))
+                        print(colorama.Fore.CYAN + "Ignoring folder in .gitignore: ", os.path.join(dirpath, dirname))
 
-                sync_file = True
+                ignored_directories.extend(new_ignored_dicts)
+                ignored_dirs_stack.append(len(new_ignored_dicts))
 
-                # Check if file is in a gitignore
-                for gitignore_match in applicable_gitignores:
-                    if gitignore_match.match_file(file_local_path):
-                        print(colorama.Fore.CYAN + "Ignoring file in .gitignore: ", file_relative_path)
+                for filename in filenames:
+                    sync_file = True
+
+                    if spec.match_file(filename):
+                        print(colorama.Fore.CYAN + "Ignoring file in .gitignore: ", os.path.join(dirpath, filename))
                         sync_file = False
                         break
 
-                if sync_file:
-                    modified_timestamp = os.path.getmtime(file_local_path)
-                    # Convert the timestamp to a datetime object
-                    modified_dt = datetime.datetime.fromtimestamp(modified_timestamp)
-                    modified_dt = modified_dt.replace(microsecond=0)
-                    # Format the datetime object as a string
-                    modified_formatted = modified_dt.strftime(TIMESTAMP_FORMAT)
+                    if sync_file:
+                        self._sync_file(file_local_path, file_relative_path, synced_paths)
+            else:
+                applicable_gitignores = self._get_applicable_gitignores(dirpath, gitignore_stack)
 
-                    if file_relative_path in synced_paths:
-                        modified_synced = datetime.datetime.strptime(synced_paths[file_relative_path], TIMESTAMP_FORMAT)
-                        if modified_synced < modified_dt:
-                            success = self.dbx_model.api_upload_file(file_local_path, file_relative_path)
-                            if success:
-                                synced_paths[file_relative_path] = modified_formatted
-                                self._write_to_synced_paths(synced_paths)
-                        else:
-                            print(colorama.Fore.GREEN + "Didn't need to sync: ", file_relative_path, modified_formatted)
-                    else:
-                        success = self.dbx_model.api_upload_file(file_local_path, file_relative_path)
-                        if success:
-                            synced_paths[file_relative_path] = modified_formatted
-                            self._write_to_synced_paths(synced_paths)
+                for dirname in dirnames:
+                    dir_local_path = os.path.join(dirpath, dirname)
+                    for gitignore_match in applicable_gitignores:
+                        if gitignore_match.match_file(dir_local_path):
+                            ignored_directories.append(dir_local_path)
+                            print(colorama.Fore.CYAN + "Ignoring folder in .gitignore: ", dir_local_path)
+
+                for filename in filenames:
+                    # construct the full local path
+                    file_local_path = os.path.join(dirpath, filename)
+                    file_relative_path = "/" + os.path.relpath(file_local_path, self.local_root)
+
+                    sync_file = True
+
+                    # Check if file is in a gitignore
+                    for gitignore_match in applicable_gitignores:
+                        if gitignore_match.match_file(file_local_path):
+                            print(colorama.Fore.CYAN + "Ignoring file in .gitignore: ", file_relative_path)
+                            sync_file = False
+                            break
+
+                    if sync_file:
+                        self._sync_file(file_local_path, file_relative_path, synced_paths)
 
             # Remove the top .gitignore file from the stack when leaving the directory
             if gitignore_stack and not dirpath.startswith(os.path.dirname(gitignore_stack[-1][0])):
+                # print(colorama.Back.RED + dirpath + " " + gitignore_stack[-1][0])
                 gitignore_stack.pop()
+                ignored_directories = ignored_directories[:-ignored_dirs_stack.pop()]
 
         # sort paths
         synced_paths = {key:synced_paths[key] for key in sorted(synced_paths.keys())}
         self._write_to_synced_paths(synced_paths)
 
         self.refresh()
+
+    def _sync_file(self, file_local_path: str, file_relative_path: str, synced_paths: dict[str, str]):
+        modified_timestamp = os.path.getmtime(file_local_path)
+        # Convert the timestamp to a datetime object
+        modified_dt = datetime.datetime.fromtimestamp(modified_timestamp)
+        modified_dt = modified_dt.replace(microsecond=0)
+        # Format the datetime object as a string
+        modified_formatted = modified_dt.strftime(TIMESTAMP_FORMAT)
+
+        if file_relative_path in synced_paths:
+            modified_synced = datetime.datetime.strptime(synced_paths[file_relative_path], TIMESTAMP_FORMAT)
+            if modified_synced < modified_dt:
+                success = self.dbx_model.api_upload_file(file_local_path, file_relative_path)
+                if success:
+                    synced_paths[file_relative_path] = modified_formatted
+                    self._write_to_synced_paths(synced_paths)
+            else:
+                print(colorama.Fore.GREEN + "Didn't need to sync: ", file_relative_path, modified_formatted)
+        else:
+            success = self.dbx_model.api_upload_file(file_local_path, file_relative_path)
+            if success:
+                synced_paths[file_relative_path] = modified_formatted
+                self._write_to_synced_paths(synced_paths)
 
     @staticmethod
     def _write_to_synced_paths(synced_paths: dict):
